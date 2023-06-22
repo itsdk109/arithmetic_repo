5b411eb00ae2422a4baabfd333c0100dfe8761a8
itsdk109 <dkjob109@gmail.com>
2023-06-23 05:06:04 +0530

    correcting program

--- add_sub_mul_div.go
+++ add_sub_mul_div.go
@@ -1,15 +1,15 @@
 package main
 
 import "fmt"
-
 func main() {
+
 var	a int = 40
 var	b int = 20
 
 var	c int = a+b
-var	c int = a-b
-var	c int= a*b
-var	c int= a/b
+var	d int = a-b
+var	e int= a*b
+var	f int= a/b
 
-fmt.Println(c)
+fmt.Println(c,d,e,f)
 }
