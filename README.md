cbd2d6d9279611a046d03a57e3d4049f7eafae2a
itsdk109 <dkjob109@gmail.com>
2023-06-23 04:59:27 +0530

    first commit for arithmetic program

---
+++ README.md
@@ -0,0 +1,1 @@
+# my first github arithmetic program
---
+++ add_sub_mul_div.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+var	a int = 40
+var	b int = 20
+
+var	c int = a+b
+var	c int = a-b
+var	c int= a*b
+var	c int= a/b
+
+	fmt.Println(c)
+}
