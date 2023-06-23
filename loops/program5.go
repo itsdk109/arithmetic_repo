f13699bd2eb12ed4e443b686dd3dcb1f3e5cfaa0
itsdk109 <dkjob109@gmail.com>
2023-06-23 15:29:41 +0530

    another for loop program

---
+++ loops/program5.go
@@ -0,0 +1,29 @@
+/* To Print this pattern -
+
+		*
+		**
+		***
+		****
+		*****
+		******
+		*******
+
+*/
+
+// Main Code Start 
+
+package main 
+
+import "fmt"
+
+func main(){
+	n := 7
+	
+	for i := 0 ; i < n ; i++ {
+
+		for j := 0 ; j <= i; j++{
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
