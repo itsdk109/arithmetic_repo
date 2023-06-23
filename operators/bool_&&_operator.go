03f2b77b43e2f1446a0f59298f7f1b8e07f1511f
itsdk109 <dkjob109@gmail.com>
2023-06-24 04:01:40 +0530

    this is Logical Operator

---
+++ operators/bool_&&_operator.go
@@ -0,0 +1,15 @@
+// Bool : it holds value with two states : True or False.
+// && Operator: it returns only True when both a and b are true.
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	a := true
+	b := false
+	c := a && b
+	d := a || b
+	fmt.Println(c)  // it returns false 
+}
