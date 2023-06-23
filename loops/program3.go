1c4505dbf90501b2334a001abcfd45bd8d1432db
itsdk109 <dkjob109@gmail.com>
2023-06-23 14:48:42 +0530

    Adding another for loop program

---
+++ loops/program3.go
@@ -0,0 +1,17 @@
+//display n terms of natural numbers and their sum
+package main 
+
+import "fmt"
+
+func main() {
+var sum int = 0
+var numbers int 
+fmt.Print("Enter the number: ")
+fmt.Scan(&numbers)
+
+for i := 1; i<=numbers; i++ {
+	fmt.Println(i)
+	sum = sum + i
+}
+	fmt.Print("Total Sum of N terms of natural numbers : ", sum)
+}
