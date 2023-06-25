3fd2654999920dc583b09b6c1106e942b4951bb0
tsdk109 <dkjob109@gmail.com>
2023-06-25 06:18:31 +0530

    Declaring a variable with an initial value

---
+++ variables/initValue_var.go
@@ -0,0 +1,13 @@
+/*
+	 Variable:
+		syntax: var variable_name type(datatype) = intialValue
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 20 //InitialValue
+	fmt.Println("InitialValue of age is: ", age)
+
+}
