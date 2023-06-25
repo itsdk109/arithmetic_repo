3f0d7147ad740f6f7005db4b35e2179d3c38c489
tsdk109 <dkjob109@gmail.com>
2023-06-25 15:17:25 +0530

    Declare multiple Variable in one line

---
+++ variables/multiple_variable.go
@@ -0,0 +1,16 @@
+/* How to Declare Multiple Variable:
+	syntax: var name1, name2 type = initialvalue1, initialvalue2 
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+var name, b string = "Mr.", "Hello_World"
+var x, y int = 25, 36
+
+fmt.Println(name, b)
+fmt.Println(x,y)
+}
