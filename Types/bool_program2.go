b10f54137239a5be6a4a4db661defc35a6c4ec4f
tsdk109 <dkjob109@gmail.com>
2023-06-27 04:28:20 +0530

    Logical OR Program

--- Types/bool_program2.go
+++ Types/bool_program2.go
@@ -1,4 +1,7 @@
-// Logical OR (||): Returns true if one of the statements is true
+/* Logical OR (||): Returns true if one of the statements is true. 
+it means Only one statement should be true. if the both statements are false.
+it will produce false.
+*/
 
 package main
 
@@ -10,5 +13,8 @@ func main(){
 
 	var e = a > b || a < b    
 	fmt.Println(e)
+
+	var f = a == 12 || a > b  // Output: False because both statements are false.
+	fmt.Println(f)
 
 }
