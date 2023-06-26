ae8770fd756c116a0186fe69537c672c4bffd449
tsdk109 <dkjob109@gmail.com>
2023-06-26 19:46:21 +0530

    Boolean Program

---
+++ Types/bool_program1.go
@@ -0,0 +1,15 @@
+// Boolean: A bool type represents a boolean and is either true or false. 
+
+// Code start Here
+
+package main
+
+import "fmt"
+
+func main(){
+	var a  = true
+	var b  = false
+
+	fmt.Println("A:",a ,"\nB:",b)
+
+}
