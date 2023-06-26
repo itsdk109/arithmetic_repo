6548df246f12f8907fa7ad6c263153149bdc5825
tsdk109 <dkjob109@gmail.com>
2023-06-26 11:48:17 +0530

    Hello World Program

---
+++ hello_world.go
@@ -0,0 +1,17 @@
+// This is the first Hello World Program in Go language.
+
+// declare "main" package using package keyword.
+package main	
+
+// fmt: fmt is a Go package that is used to format basic strings, values, inputs, and outputs 
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello World")
+
+	var hw = "Hello World"
+	fmt.Println(hw)
+
+	s := "Hello World"
+	fmt.Println(s)
+}
