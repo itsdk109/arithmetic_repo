e0a8e9ff8a4304b7d2ea4b7426347a8fb0705112
tsdk109 <dkjob109@gmail.com>
2023-06-26 19:52:59 +0530

    Logical OR Operator

---
+++ Types/bool_program2.go
@@ -0,0 +1,14 @@
+// Logical or (||): Returns true if one of the statements is true
+
+package main
+
+import "fmt"
+
+func main(){
+
+	var a, b = 14, 25
+
+	var e = a > b || a < b    
+	fmt.Println(e)
+
+}
