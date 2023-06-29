171f056b1f06c5dd2e4ac7df08b49e6411ad763f
tsdk109 <dkjob109@gmail.com>
2023-06-30 04:56:27 +0530

    No changes

--- consts.go
+++ consts.go
@@ -1,11 +1,12 @@
-package main 
+package main
+
 import "fmt"
 
 func main() {
 	const a = 10
 	fmt.Println(a)
 
-	// trying to change const value 
-	 a = 45
-	 fmt.Println(a) // error: ./consts.go:9:3: cannot assign to a (untyped int constant 10)
+	// trying to change const value
+	a = 45
+	fmt.Println(a) // error: ./consts.go:9:3: cannot assign to a (untyped int constant 10)
 }
