4eb962c3866fabd8ac901f941915f84f4f70dd24
tsdk109 <dkjob109@gmail.com>
2023-07-19 15:14:04 +0530

    How to multiple method with one struct

---
+++ method_program2.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// create a struct
+type calculator struct {
+	num1, num2 int
+}
+
+func (c calculator) sum() int {
+
+	return c.num1 + c.num2
+}
+
+func (c calculator) sub() int {
+
+	return c.num1 - c.num2
+}
+
+func (c calculator) mul() int {
+
+	return c.num1 * c.num2
+}
+
+func (c calculator) div() int {
+
+	return c.num1 / c.num2
+}
+
+func main() {
+	call_calculator := calculator{44, 5}
+	fmt.Println("Sum of two number is: ", call_calculator.sum())
+	fmt.Println("Subtract of two number is: ", call_calculator.sub())
+	fmt.Println("Multiply of two number is: ", call_calculator.mul())
+	fmt.Println("Division of two number is: ", call_calculator.div())
+}
